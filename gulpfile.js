1dfd889951d2dcffa9056f8535c7a5b33d6fb135
Simon Thum <simon.thum@igd.fraunhofer.de>
2014-05-28 11:18:46 +0200

    add JSHint
    ...and fix some build issues to make linting easier

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,21 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var jshintStylish = require('jshint-stylish');
+var using = require('gulp-using');
 
+// gulp-order spectacularly failed to do anything,
+// so we list files here explicitly.
 var paths = {
-  source_scripts: ['src/js/**/*.js']
+  source_scripts: ['src/js/singleton.js',
+                   'src/js/init.js',
+                   'src/js/*.js']
 };
 
 gulp.task('javascript-min', function() {
   // Minify and copy all JavaScript (except vendor scripts)
   return gulp.src(paths.source_scripts)
-    .pipe(uglify())
     .pipe(concat('gis3dom.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('dist'))
     .pipe(gulp.dest('public/javascripts'));
 });
@@ -29,10 +34,33 @@ gulp.task('javascript', function() {
 gulp.task('lintjs', function() {
   // Minify and copy all JavaScript (except vendor scripts)
   return gulp.src(paths.source_scripts)
-    .pipe(jshint())
+    .pipe(using())
+    .pipe(concat('gis3dom.js'))
+    .pipe(jshint({
+        "bitwise":true,
+        "camelcase":true,
+        "curly":true,
+        "eqeqeq":true,
+        "forin":true,
+        "freeze":true,
+        "immed":true,
+        "latedef":"nofunc",
+        "newcap":true,
+        "noempty":true,
+        "nonbsp":true,
+        "nonew":true,
+        "undef":true,
+        "unused":true,
+        // undecided on the below
+        // "strict":true,
+        "browser":true,
+        "jquery":true
+    }))
     .pipe(jshint.reporter(jshintStylish));
+    // TODO append when silent
+    // .pipe(jshint.reporter('fail'))
 });
 
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['javascript', 'javascript-min', 'lintjs']);
+gulp.task('default', ['lintjs', 'javascript', 'javascript-min']);
